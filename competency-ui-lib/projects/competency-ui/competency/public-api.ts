e716d72e365fa640343b20d85d72420a8b4ec93a
Vishali Sakar <vishali.sakar@tarento.com>
2022-11-17 16:00:39 +0530

    competency Dashboard added

--- competency-ui-lib/projects/competency-ui/competency/public-api.ts
+++ competency-ui-lib/projects/competency-ui/competency/public-api.ts
@@ -1,3 +1,4 @@
 export * from './components/required-comptency-card/required-comptency-card.component'
 export * from './components/gained-comptency-card/gained-comptency-card.component'
+export * from './components/competency-dashboard/competency-dashboard.component'
 export * from './competency.module'
