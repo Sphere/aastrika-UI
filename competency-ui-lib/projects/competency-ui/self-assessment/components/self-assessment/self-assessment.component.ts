692748907ffdd5d4fa44898b09b2e7e2d1f7fe3a
mansurskTarento <mansur.shaik@tarento.com>
2022-11-21 18:13:47 +0530

    added navigat back, gained tab new ui changes, required tab self assessment button ui adjust

--- competency-ui-lib/projects/competency-ui/self-assessment/components/self-assessment/self-assessment.component.ts
+++ competency-ui-lib/projects/competency-ui/self-assessment/components/self-assessment/self-assessment.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common'
 
 @Component({
   selector: 'lib-self-assessment',
@@ -20,9 +21,15 @@ export class SelfAssessmentComponent implements OnInit {
     }
   ]
 
-  constructor() { }
+  constructor(
+    private location: Location
+  ) { }
 
   ngOnInit() {
+  }
+
+  navigateBack() {
+    this.location.back()
   }
 
 }
