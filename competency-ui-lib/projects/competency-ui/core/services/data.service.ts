ea266dda9c6cb8df18d5ab1c78f92c3f4b7986a7
amantarento <amankumar.sharma@tarento.com>
2022-11-23 16:40:43 +0530

    add the core module

---
+++ competency-ui-lib/projects/competency-ui/core/services/data.service.ts
@@ -0,0 +1,74 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {mergeMap} from 'rxjs/operators'
+import { of as observableOf, throwError as observableThrowError, Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+    /**
+   * Contains base Url for api end points
+   */
+     baseUrl: string;
+    /**
+   * angular HttpClient
+   */
+  http: HttpClient;
+  constructor(http:HttpClient) {
+    this.http = http
+   }
+  /**
+   * for making get api calls
+   *
+   * @param requestParam interface
+   */
+  get(requestParam:any):Observable<any>{
+    const httpOptions: any = {
+      headers: requestParam.header ? requestParam.header : this.getHeader(),
+      params: requestParam.param,
+      observe: 'response'
+    };
+    return this.http.get(this.baseUrl + requestParam.url, httpOptions).pipe(
+      mergeMap(({ body, headers }: any) => {
+        if (body.responseCode !== 'OK') {
+          return observableThrowError(body);
+        }
+        return observableOf(body);
+      }));
+  }
+   /**
+   * for making get api calls
+   *
+   * @param requestParam interface
+   */
+  post(requestParam:any){
+    const httpOptions:any = {
+      headers: requestParam.header ? this.getHeader(requestParam.header) : this.getHeader(),
+      params: requestParam.param
+    }
+    return this.http.post(this.baseUrl+requestParam.url,requestParam.data,httpOptions).pipe(
+      mergeMap((data:any)=>{
+        if (data.responseCode !== 'OK') {
+          return observableThrowError(data);
+        }
+        return observableOf(data);
+      })
+    )
+  }
+  /**
+   * for preparing headers
+   */
+   private getHeader(headers?: any): any {
+    
+    const default_headers = {
+      'Accept': 'application/json',
+     
+    };
+    
+    if (headers) {
+      return { ...default_headers, ...headers };
+    } else {
+      return { ...default_headers };
+    }
+  }
+}
