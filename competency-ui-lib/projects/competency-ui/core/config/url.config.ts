04c2815e07955a01617e42f74b05cf700b003913
amantarento <amankumar.sharma@tarento.com>
2022-11-24 20:33:47 +0530

    change the host

--- competency-ui-lib/projects/competency-ui/core/config/url.config.ts
+++ competency-ui-lib/projects/competency-ui/core/config/url.config.ts
@@ -1,7 +1,7 @@
 export const urlConfig = {
   // endpoint configs...!
   // host: 'http://localhost:3002',
-  host: 'https://sphere.aastrika.org',
+  host: 'http://localhost:3000',
   apiSlug: '/apis/protected/v8',
   apiBasePath: () => `${urlConfig.host}${urlConfig.apiSlug}`,
   getEntityById: (id:number) => `${urlConfig.apiBasePath()}/entityCompetency/getEntityById/${id}`,
