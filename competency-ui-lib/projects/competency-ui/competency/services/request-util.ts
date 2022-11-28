1a095107ac40eaef8b628b7328aa18cd7aa3241a
amantarento <amankumar.sharma@tarento.com>
2022-11-28 16:19:49 +0530

    push the changes for date filter

--- competency-ui-lib/projects/competency-ui/competency/services/request-util.ts
+++ competency-ui-lib/projects/competency-ui/competency/services/request-util.ts
@@ -64,11 +64,125 @@ export class RequestUtil {
     return result
   }
 
-  formatedGainedAll = (data:any) => {
-    if(!_.isEmpty(data)){
-        return data
+  /**
+ * util method to formate the gained competency  
+ * for user 
+ */
+
+  formatedGainedCompetency(entity:any, passbook:any){
+    let response = []
+    console.log(entity)
+    console.log(passbook)
+    _.forEach(entity,(value:any)=>{
+        const cid =   _.get(value, 'id')
+        _.forEach(passbook,(passbookValue:any)=>{
+           if(passbookValue.competencies.hasOwnProperty(cid)){
+              console.log('inside if')
+              const competency =  passbookValue.competencies[cid]
+              response.push({
+                'title': _.get(competency,'additionalParams.competencyName'),
+                'logs': this.acquiredPassbookLogs(_.get(competency, 'acquiredDetails')),
+                'proficiencyLevels': this.acauiredChannelColourCode(_.get(competency, 'acquiredDetails'))
+              })
+              
+            }
+        })
+    })
+    
+    console.log(response)
+    return response
+  }
+  acquiredPassbookLogs(acquiredDetails:any){
+    let response  = []
+    if(acquiredDetails.length>0){
+      _.forEach(acquiredDetails,(value:any)=>{
+          response.push({
+            'header': _.get(value, 'courseName') ? _.get(value, 'courseName') : '',
+            'date':  _.get(value,'createdDate'),
+            'description': _.get(value, 'additionalParams.description'),
+            'keyboardArrowUp':true
+          })
+      })
     }
-}
+   return response
+  }
+  acauiredChannelColourCode(acquiredDetails:any){
+    let response  = [
+     {
+       'color': '#FFFBB0',
+       'displayLevel': 1,
+       'selected': false,
+     },
+     {
+       'color': '#FFFBB0',
+       'displayLevel': 2,
+       'selected': false,
+     },
+     {
+       'color': '#FFFBB0',
+       'displayLevel': 3,
+       'selected': false,
+     },
+     {
+       'color': '#FFFBB0',
+       'displayLevel': 4,
+       'selected': false,
+     },
+     {
+       'color': '#FFFBB0',
+       'displayLevel': 5,
+       'selected': false,
+     }
+    ]
+   
+   _.forEach(acquiredDetails,(value:any)=>{
+       const channel = _.get(value,'acquiredChannel')
+       switch(channel) {
+         case 'Course':{
+           _.forEach(response, (level:any)=>{
+             if(level.displayLevel == _.get(value,'competencyLevelId')){
+               level.color = '#DFEDF9';
+               level.selected = true
+             }
+           } )
+           
+           break; 
+         }
+         case 'self Assessment':{
+           _.forEach(response, (level:any)=>{
+             if(level.displayLevel == _.get(value,'competencyLevelId')){
+               level.color = '#FFF4DF';
+               level.selected = true
+               
+             }
+           } )
+         
+           break; 
+         }
+         case 'Admin':{
+           _.forEach(response, (level:any)=>{
+             if(level.displayLevel == _.get(value,'competencyLevelId')){
+               level.color = '#A4DFCA';
+               level.selected = true
+             }
+           } )
+           
+           break; 
+         }
+         default: { 
+           _.forEach(response, (level:any)=>{
+             if(level.displayLevel == _.get(value,'competencyLevelId')){
+               level.color = '#FFFBB0';
+               level.selected = false;
+             }
+           } )
+           
+           break; 
+         }
+       }
+   })
+   return response
+ }
 }
 
 
