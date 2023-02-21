b4930b85396698135cff9cf3495ed45eae89d3f8
Vishali Sakar <vishali.sakar@tarento.com>
2023-02-21 18:18:27 +0530

    search navigation fixes

--- competency-ui-lib/projects/competency-ui/self-assessment/components/self-assessment-card/self-assessment-card.component.ts
+++ competency-ui-lib/projects/competency-ui/self-assessment/components/self-assessment-card/self-assessment-card.component.ts
@@ -26,48 +26,56 @@ export class SelfAssessmentCardComponent implements OnInit {
   }
 
   viewCourses(data) {
-    console.log(data);
-    let reqBody;
-     if (data.competencyID) {
-      reqBody = {
-        "request": {
-          "filters": {
-            "competencySearch": [
-              `${data.competencyID}-1`,
-              `${data.competencyID}-2`,
-              `${data.competencyID}-3`,
-              `${data.competencyID}-4`,
-              `${data.competencyID}-5`
-            ],
-              "primaryCategory": [
-                "Course"
-              ],
-                "contentType": [
-                  "Course"
-                ],
-                  "status": [
-                    "Live"
-                  ]
-          },
-          "sort_by": {
-            "lastUpdatedOn": "desc"
-          }
-        },
-        "sort": [
-          {
-            "lastUpdatedOn": "desc"
-          }
-        ]
-      }
 
-      this.selfAssessmentService.getCompetencyCourseIdentifier(reqBody).subscribe(
-        res => {
-          this.router.navigate(['/app/search'], {
-            queryParams: { q: res.result.content[0].name },
-            queryParamsHandling: 'merge',
-          })
-        }
-      )
+     if (data.competencyID) {
+
+      this.router.navigate(['/app/search'], {
+        queryParams: { q: [
+          `${data.competencyID}-1`,
+          `${data.competencyID}-2`,
+          `${data.competencyID}-3`,
+          `${data.competencyID}-4`,
+          `${data.competencyID}-5`
+        ] , competency: true},
+        queryParamsHandling: 'merge',
+      })
+      // reqBody = {
+      //   "request": {
+      //     "filters": {
+      //       "competencySearch": [
+      //         `${data.competencyID}-1`,
+      //         `${data.competencyID}-2`,
+      //         `${data.competencyID}-3`,
+      //         `${data.competencyID}-4`,
+      //         `${data.competencyID}-5`
+      //       ],
+      //         "primaryCategory": [
+      //           "Course"
+      //         ],
+      //           "contentType": [
+      //             "Course"
+      //           ],
+      //             "status": [
+      //               "Live"
+      //             ]
+      //     },
+      //     "sort_by": {
+      //       "lastUpdatedOn": "desc"
+      //     }
+      //   },
+      //   "sort": [
+      //     {
+      //       "lastUpdatedOn": "desc"
+      //     }
+      //   ]
+      // }
+
+      // this.selfAssessmentService.getCompetencyCourseIdentifier(reqBody).subscribe(
+      //   res => {
+
+          
+      //   }
+      // )
     }
   }
 }
