64a5d13f90c08737ce28cfd3fc2ca806f3e22096
Vishali Sakar <vishalisakar27@gmail.com>
2023-04-06 14:20:11 +0530

    Search API updated

--- competency-ui-lib/projects/competency-ui/core/config/url.config.ts
+++ competency-ui-lib/projects/competency-ui/core/config/url.config.ts
@@ -14,7 +14,7 @@ export const urlConfig = {
   getEntityById: (id:number) => `${urlConfig.apiBasePath()}/entityCompetency/getEntityById/${id}`,
   getAllEntity: () => `${urlConfig.apiBasePath()}/entityCompetency/getAllEntity`,
   getUserPassbook: () => `${urlConfig.apiBaseProxy()}/user/v1/passbook`,
-  getSearch:() => `${urlConfig.apiBasePublic()}/publicContent/v1/search`,
+  getSearch:() => `${urlConfig.apiBasePublic()}/publicSearch/getCourses`,
   getHierachyDetails:(identifier, hierarchyType) => `${urlConfig.apiBaseProxy()}/action/content/v3/hierarchy/${identifier}?hierarchyType=${hierarchyType}`,
   getUserdetailsFromRegistry:(wid: string)=> `${urlConfig.apiBaseProxy()}/api/user/v2/read/${wid}`,
   getContentProgress: (id) => `${urlConfig.apiBaseProxy()}/read/content-progres/${id}`
