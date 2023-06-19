aba0f958143347b5560d92b36eedfa1df023ade3
amantarento <amankumar.sharma@tarento.com>
2023-06-19 13:19:24 +0530

    added mobile host

--- competency-ui-lib/projects/competency-ui/core/config/url.config.ts
+++ competency-ui-lib/projects/competency-ui/core/config/url.config.ts
@@ -28,6 +28,6 @@ export const urlConfig = {
   getSearchMobile: () => `${urlConfig.mobileHost}${urlConfig.apiPublic}/publicSearch/getCourses`,
   getContentProgressMobile: () => `${urlConfig.mobileHost}/api/course/v1/content/state/read`,
   
-  getRoleMapping:() => `${urlConfig.apiBasePublic()}/competencyAssets/rolesMappingData`
+  getRoleMapping:() => `${urlConfig.mobileHost}${urlConfig.apiPublic}/competencyAssets/rolesMappingData`
 
 }
