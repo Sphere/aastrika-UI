b1d894beaed5be4ebf6f4d06b31eb45b95383baa
amantarento <amankumar.sharma@tarento.com>
2024-10-08 13:33:56 +0530

    fix the issue of angular 10 migration

--- competency-ui-lib/projects/competency-ui/public-api.ts
+++ competency-ui-lib/projects/competency-ui/public-api.ts
@@ -3,12 +3,14 @@
  */
 
 
-export * from './competency/competency.module'
-export * from './competency/components/required-comptency-card/required-comptency-card.component'
-export * from './competency/components/active-summary/active-summary.component'
-export * from './competency/components/gained-comptency-card/gained-comptency-card.component'
-export * from './competency/components/competency-dashboard/competency-dashboard.component'
-export * from './competency/components/active-summary/active-summary.component'
+// export * from './competency/competency.module'
+// export * from './competency/components/required-comptency-card/required-comptency-card.component'
+// export * from './competency/components/active-summary/active-summary.component'
+// export * from './competency/components/gained-comptency-card/gained-comptency-card.component'
+// export * from './competency/components/competency-dashboard/competency-dashboard.component'
+// export * from './competency/components/active-summary/active-summary.component'
 
 
-// export const MY_COMP_UI_SDK = "@aastrika_npmjs/comptency";
+
+
+ export const MY_COMP_UI_SDK = "@aastrika_npmjs/comptency";
