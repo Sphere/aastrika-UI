0df3bcbade3cf16b459886ad1d7a66fecf01f634
amantarento <amankumar.sharma@tarento.com>
2024-10-10 11:40:45 +0530

    angular 10 optimization

--- competency-ui-lib/projects/competency-ui/self-assessment/self-assessment.module.ts
+++ competency-ui-lib/projects/competency-ui/self-assessment/self-assessment.module.ts
@@ -1,4 +1,4 @@
-import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA, NgModule  } from '@angular/core';
+import {  NgModule  } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SelfAssessmentCardComponent } from './components/self-assessment-card/self-assessment-card.component';
 import { SelfAssessmentComponent } from './components/self-assessment/self-assessment.component';
